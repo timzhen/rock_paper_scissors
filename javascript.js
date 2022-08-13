75c6f827bb7c8c7f06e5ca02f6981d2b521a3223
timzhen <tim.zhen21@gmail.com>
2022-08-13 12:42:35 -0700

    Check player's in-game choice and only allows player to choose game options

--- javascript.js
+++ javascript.js
@@ -1,6 +1,46 @@
-function getComputerChoice () {
-    const option = ['Rock','Paper','Scissors']
+const option = ['rock', 'paper', 'scissors']
+
+function getComputerChoice () { // computer seleciton
     var random = option[Math.floor(Math.random()*option.length)];
-    console.log (random)
+    console.log ('Computer chose', random)
 }
-getComputerChoice();
+
+function playerChoice () { //player selection
+ let input = prompt ('Rock, Paper, Scissors')
+    input = input.toLowerCase();
+
+ while (input == null) {
+    input = prompt ('Rock, Paper, Scissors')
+ }
+   let check = checkChoice (input)
+ while (check == false) {
+   input = prompt ('Please choose rock, paper, or scissors (spelling matters)')
+ while (input == null) {
+   input = prompt ('Rock, Paper, Scissors')
+ }
+    input = input.toLowerCase()
+    check = checkChoice(input); 
+    console.log ('You chose', input)
+ }
+
+}    
+playerChoice()
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection == option [0] && computerSelection == option [1]) {
+        console.log ('You Lose! Paper beats Rock')
+    }
+    else if (playerSelection == option [0] && computerSelection == option [2]) {
+        console.log ('You Win! Rock beats Scissors')
+    }
+    else if (playerSelection == option [0] && computerSelection == option [0]) { 
+        console.log ('No one wins')}
+}
+
+function checkChoice (choice) {
+    if (option.includes(choice)) {
+        return true
+    } else {
+        return false
+    }
+}
+//playRound ('rock', 'paper')
