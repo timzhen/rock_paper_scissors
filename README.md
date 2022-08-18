b5db56c61dba44e2f3863e5c143b44d66f606041
timzhen <tim.zhen21@gmail.com>
2022-08-17 17:34:01 -0700

    add round count, display winner of round, display score of game

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rock_paper_scissors
+
+pseudocode:
+    1. when user wins a round, add a point for player
+    2. when no one wins, no points added
+    3. when computer wins a round, add a point of computer
+    4. when either side wins 5 points, wins overall game
--- javascript.js
+++ javascript.js
@@ -1,16 +1,34 @@
 const option = ['rock', 'paper', 'scissors']
+const winners = []
+
+function playRound (round) {
+    const playerSelection = playerChoice();
+    const computerSelection = getComputerChoice();
+    const winner = checkWinner(playerSelection, computerSelection)
+    //console.log (winner)
+    winners.push (winner)
+    logRound(playerSelection, computerSelection, winner, round)
+}
+
+function game () { //plays game rounds, first to 5
+    for (let i = 1; i <= 5; i++ ) {
+        playRound(i)
+    }
+    logWins()
+}
 
 function getComputerChoice () { // computer seleciton
     var random = option[Math.floor(Math.random()*option.length)];
-    console.log ('Computer chose', random)
+    //console.log ('Computer chose', random)
+    return random;
 }
 
 function playerChoice () { //player selection
- let input = prompt ('Rock, Paper, Scissors')
+ let input = prompt ('Type Rock, Paper, Scissors')
     input = input.toLowerCase();
 
  while (input == null) {
-    input = prompt ('Rock, Paper, Scissors')
+    input = prompt ('Type Rock, Paper, Scissors')
  }
    let check = checkChoice (input)
  while (check == false) {
@@ -19,21 +37,20 @@ function playerChoice () { //player selection
    input = prompt ('Rock, Paper, Scissors')
  }
     input = input.toLowerCase()
-    check = checkChoice(input); 
-    console.log ('You chose', input)
+    check = checkChoice(input);     
  }
+//console.log ('You chose', input)
+return input;
+}
 
-}    
-playerChoice()
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection == option [0] && computerSelection == option [1]) {
-        console.log ('You Lose! Paper beats Rock')
+function checkWinner (playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper') {
+        return ('You Win')
+    } else if (playerSelection === computerSelection) {
+        return('tie')
+    } else {
+        return ('You Lose')
     }
-    else if (playerSelection == option [0] && computerSelection == option [2]) {
-        console.log ('You Win! Rock beats Scissors')
-    }
-    else if (playerSelection == option [0] && computerSelection == option [0]) { 
-        console.log ('No one wins')}
 }
 
 function checkChoice (choice) {
@@ -43,4 +60,23 @@ function checkChoice (choice) {
         return false
     }
 }
-//playRound ('rock', 'paper')
+
+function logWins () {
+    let playerWins = winners.filter ((item) => item == 'You Win').length
+    let computerWins = winners.filter ((item) => item == 'You Lose').length
+    let ties = winners.filter ((item) => item == 'tie').length
+    console.log ('Results:')
+    console.log ('Player Wins:', playerWins)
+    console.log ('Computer Wins:', computerWins)
+    console.log ('Ties', ties)
+}
+
+function logRound (playerSelection, computerSelection, winner, round) {
+    console.log ('Round',round)
+    console.log ('player choses', playerSelection)
+    console.log ('computer choses',computerSelection)
+    console.log (winner, 'won this round')
+    console.log ('---------------------------------------------------------')
+}
+
+game()
